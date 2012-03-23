57380b378b580276c43ad5f42d33f4d0930cc396
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-03-22 21:57:25 -0300

    Be less restrictive on actionpack dependency to allow any Rails 3.x version
    Also add missing require to sprockets to work with current versions.
    Tested with actionpack 3.2.2 and sprockets 2.1.2.

--- lib/sprockets/jquery/tmpl/tmpl_processor.rb
+++ lib/sprockets/jquery/tmpl/tmpl_processor.rb
@@ -1,3 +1,4 @@
+require 'sprockets'
 require 'sprockets/engines'
 require 'tilt'
 require 'action_view'
@@ -27,6 +28,6 @@ module Sprockets
       end
     end
   end
-  
+
   register_engine '.tmpl', ::Sprockets::Jquery::Tmpl::TmplProcessor
 end
